a5f313e10f94ea6a6f65c076b20f00e9c48dc380
Kumar Dokuparthi, Naveen <naveen.dokuparthi@cdk.com>
2017-06-06 17:48:09 -0500

    Adding /heath api which returns {'status':'up'}

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ var app = express()
 app.get('/', function (req, res) {
   res.send('append /index.html to the url and hit enter')
 })
+
+app.get('/health', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({'status': 'up'}))
+})
  
 app.use(express.static(__dirname))
 
