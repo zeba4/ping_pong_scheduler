fc853466077978d5b4a70dcc9f9d1f933d8c093c
DS\mroza <Arden.Mroz@cdk.com>
2017-06-21 14:43:41 -0500

    fixed typo and reformatted for clarity

--- master.js
+++ master.js
@@ -1,9 +1,8 @@
 
 var totalTime;
-var designatedStart_time =
-localStorage.setItem('startTime', designatedStart_time)
+var flag=true;
+var designatedStart_time = localStorage.setItem('startTime', designatedStart_time)
 var countSec;
-
 function start20()
 {
   totalTime=1200;
@@ -21,20 +20,16 @@ function start30()
   totalTime = 1800;
   countSec=setInterval(function(){minuteTimer(totalTime,"3")}, 1000);
 }
-var flag=true;
 function minuteTimer(time, uid) //decreses the seconds value
 {
   $("#button1").unbind("click", startTenMinuteTimer);
   $("#button2").unbind("click", startTwentyMinuteTimer);
   $("#button3").unbind("click", startThirtyMinuteTimer);
-
-
   var minutes=Math.floor(time/60);
   var seconds=Math.floor(time%60);
   if(seconds<10)
   {
     seconds="0"+ seconds
-
   }
 
   if (minutes==1 && seconds<59)
@@ -42,32 +37,20 @@ function minuteTimer(time, uid) //decreses the seconds value
   {
     if(flag)
     {
-
       showTimerWarning();
-
       document.getElementById('audio').play();
       flag=false;
     }
-
   }
-
-
   document.getElementById("timer" + uid).innerHTML=minutes +":" +seconds;
-
-
-  if ((time < 0))
-  {
-   clearInterval(countSec);
+if ((time < 0))
+  { clearInterval(countSec);
    hideTimerWarning();
-
    $("#button1").bind("click", startTenMinuteTimer);
    $("#button2").bind("click", startTwentyMinuteTimer);
    $("#button3").bind("click", startThirtyMinuteTimer);
    document.getElementById('audio2').play();
    return;
-
- }
-
- totalTime=totalTime-1;
-
+    }
+totalTime=totalTime-1;
 }
